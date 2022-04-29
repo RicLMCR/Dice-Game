961b2fe2ca46c750a2b4c6631f58e26c7b9cbeb7
RicLMCR <99919289+RicLMCR@users.noreply.github.com>
2022-04-29 12:12:14 +0100

    Delete randomNumEventListener test.js

--- randomNumEventListener test.js
+++
@@ -1,7 +0,0 @@
-// const button = document.getElementById("player1Btn")
-// const p1Score = document.getElementById("score1")
-// player1Btn.addEventListener("click", (event)=>{//Looks out for the click event within an element 
-//     let player1Score = Math.floor(Math.random()*5 + 1);
-//     console.log(player1Score);
-//     p1Score.textContent = player1Score;
-// })
